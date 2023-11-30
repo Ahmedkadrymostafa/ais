dc9db1263be395b7608a8e4df34fd8dd49e0faa1
Ahmedkadrymostafa <ahmedkadry730@gmail.com>
2023-11-30 21:21:04 +0200

    "first commit"

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// toggler burger btn 
+let sideSection = document.querySelector(".header-links")
+let sideBtn = document.querySelector(".burger-btn")
+function toggler() {
+    sideSection.classList.toggle("active")
+    sideBtn.classList.toggle("active")
+}
+// end toggler burger btn 
